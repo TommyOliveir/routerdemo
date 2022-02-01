04d1d6ab8854ee6932b9dabc05af0f8950c326e3
Tommy Oliveir <laridatommy@gmail.com>
2022-02-01 19:32:12 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Navbar } from "./components/Navbar";
+import { OrderSummary } from "./components/OrderSummary";
+import { NoMatch } from "./components/NoMatch";
+import { Products } from "./components/Products";
+import { FeaturedProducts } from "./components/FeaturedProducts";
+import { NewProducts } from "./components/NewProducts";
+import { Users } from "./components/Users";
+import { UserDetails } from "./components/UserDetails";
+import { Admin } from "./components/Admin";
 
 function App() {
   return (
-    <div className="App">
-   Hello Toomy
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />}></Route>
+          <Route path="admin" element={<Admin />}></Route>
+        </Route>
+        <Route path="*" element={<NoMatch />}></Route>
+      </Routes>
+    </>
   );
 }
 
